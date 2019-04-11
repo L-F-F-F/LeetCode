204c8fa034c47f88e410a1f4375036f072b678d2
L-F-F-F <935861229@qq.com>
2019-04-11 13:32:53 +0800

    Update mid.py

--- mid.py
+++ mid.py
@@ -69,8 +69,57 @@ class Solution(object):
         # 计算组合数，总共需要走m+n-2步，m-1步横着走，n-1步竖着走，C(m-1,m+n-2)
         return self.CC(m - 1, m + n - 2)
 
+    # 63. 不同路径 II
+    def uniquePathsWithObstacles(self, obstacleGrid):
+
+        m = len(obstacleGrid)
+        if m == 0:
+            return 0
+        n = len(obstacleGrid[0])
+        if n == 0:
+            return 0
+
+        if obstacleGrid[m - 1][n - 1] == 1:
+            return 0
+        # # 递归费时
+        # if m == 1 and n == 1:
+        #     return 1
+        # if m == 1:
+        #     return self.uniquePathsWithObstacles([obstacleGrid[0][:-1]])
+        # if n == 1:
+        #     return self.uniquePathsWithObstacles(obstacleGrid[:-1])
+        # return self.uniquePathsWithObstacles([x[:-1] for x in obstacleGrid]) + self.uniquePathsWithObstacles(
+        #     obstacleGrid[:-1])
+
+        num = [[0 for i in range(n)] for j in range(m)]
+        # print(num)
+        for j in range(n):  # 第一行
+            if obstacleGrid[0][j] == 0:
+                num[0][j] = 1
+            else:
+                break
+        for i in range(m):  # 第一列
+            if obstacleGrid[i][0] == 0:
+                num[i][0] = 1
+            else:
+                break
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j - 1] == 1 and obstacleGrid[i - 1][j] == 1:
+                    num[i][j] = 0
+                elif obstacleGrid[i - 1][j] == 1:  # 上面有障碍
+                    num[i][j] = num[i][j - 1]
+                elif obstacleGrid[i][j - 1] == 1:  # 左面有障碍
+                    num[i][j] = num[i - 1][j]
+                else:
+                    num[i][j] = num[i - 1][j] + num[i][j - 1]
+        return num[m - 1][n - 1]
+
 
 if __name__ == "__main__":
     a = Solution()
-    b = a.combinationSum2([2, 3, 1], 3)
+    A = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    A[2][2] = 0
+    B = []
+    b = a.uniquePathsWithObstacles([[0, 0], [1, 1], [0, 0]])
     print(b)
